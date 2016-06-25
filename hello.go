688bc78ca4318c5e14bd82edeaea819d6cf57f21
zbzhang <zbzhang@zbzhangdeMacBook-Air.local>
2016-06-25 20:40:23 +0800

    initial commit

---
+++ hello.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+import "github.com/pythoner2012/string"
+func main(){
+	  fmt.Println(string.Reverse("hello, world"))
+          fmt.Println("true&&false=",true&&false)
+}
